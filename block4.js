f16aa85f65e68f405be1899635ecd5b88d1656e8
Nadia-HPoe <isslandia@mail.ru>
2023-09-24 21:13:42 +0300

    добавлены переменные в JS

--- block4.js
+++ block4.js
@@ -1,3 +1,18 @@
+let amountContainer = document.querySelector(".block-4__amount");
+let priorityContainer = document.querySelector(".block-4__priority");
+let deadlineContainer = document.querySelector(".block-4__deadline");
+let startDateContainer = document.querySelector(".block-4__startDate");
+let endDateContainer = document.querySelector(".block-4__endDate");
+let goalImage = document.querySelector(".block-4__goal-image");
+
+document.addEventListener("DOMContentLoaded", function () {
+  const goalJSON = localStorage.getItem("goalObj");
+  objectGoal = JSON.parse(goalJSON);
+  let amountText = document.createElement("p");
+  amountContainer.appendChild(amountText);
+  amountText.innerHTML = `${objectGoal.amount}`;
+});
+
 ///////внесение денег//////
 function Deposite() {
   const errorMessage = document.querySelector(".block-4__error-message");
