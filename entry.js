526821ed0a3a22d115eb41a72415337fc0747d72
Анастасия <doby09@mail.ru>
2023-09-26 19:46:39 +0500

    js  для страницы входа

--- entry.js
+++ entry.js
@@ -1,1 +1,33 @@
+const error = document.getElementById('errorMessage');
+const formEntrance = document.forms.formEntrance;
+const login = formEntrance.elements.login;
+const password = formEntrance.elements.password;
+
+formEntrance.addEventListener('submit', function (event) {
+  event.preventDefault();
+
+  if (login.value.trim() === '') {
+    error.textContent = "Необходимо заполнить поле логин!"
+  }
+  if (password.value.trim() === '') {
+    error.textContent = "Введите пароль!"
+  }
+  if ((login.value.trim() === '') && (password.value.trim() === '')) {
+    error.textContent = "Заполните поля!"
+  }
+  if (login.value.trim() === 'admin' && password.value.trim() === 'password') {
+    window.location.href = '/home';
+  } else {
+    error.textContent = 'Неправильное имя пользователя или пароль. Пожалуйста, попробуйте еще раз.';
+  }
+  formEntrance.reset();
+});
+
+
+
+
+
+
+
+
 
