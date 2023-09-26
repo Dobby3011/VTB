5b5bd64d11a5e3d48fd3cd1f16a2a11b8313aef5
katekoso <116917623+katekoso@users.noreply.github.com>
2023-09-26 13:09:35 +0300

    feat: add info from local storage

--- js/progress.js
+++ js/progress.js
@@ -1,20 +1,31 @@
 const goalsElement = document.querySelector('.goals');
 
+const goalString = window.localStorage.getItem('goal');
+const goal = JSON.parse(goalString);
+
 let count = 0;
+
+document.addEventListener("DOMContentLoaded", function(event) {
+    if(goal) {
+      const goalCount = createGoal();
+      window.localStorage.setItem(`goal-${goalCount}`, JSON.stringify(goal));
+      window.localStorage.removeItem('goal');
+    }
+});  
 
 function createGoal() {
   count += 1;
   const newGoal = document.createElement('div');
   newGoal.classList.add('goals__item');
-  newGoal.innerHTML = `<h4 class="goals__name">Одежда в школу</h4> <p class="goals__progress">Прогресс цели</p> <div id="progress" class="goals__progressBar"><div id="progressContent-${count}" class="goals__progressBar_content">
+  newGoal.innerHTML = `<h4 class="goals__name">${goal.name}</h4> <p class="goals__progress">Прогресс цели</p> <div id="progress" class="goals__progressBar"><div id="progressContent-${count}" class="goals__progressBar_content">
   <span class="goals__progressBar_text" id="progressText-${count}"></span>
 </div></div><div class="goals__bottom"><div class="goals__money-left"><span class="goals__left">Осталось собрать: </span><span class="goals__amount">10 тыс</span></div><button class="goals__btn" type="button">&gt;</button></div>`;
   goalsElement.append(newGoal);
   return count;
 }
 
-const goal1 = createGoal();
-const goal2 = createGoal();
+//const goal1 = createGoal();
+//const goal2 = createGoal();
 
 function updateProgressBarContent(percent, goalCount) {
   let progressBarContent = document.querySelector(`#progressContent-${goalCount}`);
@@ -30,5 +41,5 @@ function updateProgressBarContent(percent, goalCount) {
   } 
 }
 
-updateProgressBarContent(45, goal1);
-updateProgressBarContent(70, goal2);
+//updateProgressBarContent(45, goal1);
+//pdateProgressBarContent(70, goal2);
