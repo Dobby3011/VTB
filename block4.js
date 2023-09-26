932fe1facf2a441275f8a09c0cb945b8e6b3acb4
Nadia-HPoe <isslandia@mail.ru>
2023-09-26 16:47:06 +0300

    добавлена история операций в html и js

--- block4.js
+++ block4.js
@@ -6,17 +6,21 @@ let endDateContainer = document.querySelector(".block-4__endDate");
 let goalImage = document.querySelector(".block-4__goal-image");
 
 document.addEventListener("DOMContentLoaded", function () {
-  const goalJSON = localStorage.getItem("goalObj");
-  objectGoal = JSON.parse(goalJSON);
+  // const goalJSON = localStorage.getItem("goalObj");
+  // objectGoal = JSON.parse(goalJSON);
+  let amount = 50000;
   let amountText = document.createElement("p");
   amountContainer.appendChild(amountText);
-  amountText.innerHTML = `${objectGoal.amount}`;
+  amountText.classList.add("block-4__data-info");
+  amountText.innerHTML = `${amount}₽`;
 });
 
 ///////внесение денег//////
 function Deposite() {
   const errorMessage = document.querySelector(".block-4__error-message");
   const Input = document.querySelector("#block-4__deposit-input").value;
+
+  ///проверки на внесение суммы///
   if (Input === "") {
     errorMessage.textContent = "Введите сумму!";
     return;
@@ -27,14 +31,26 @@ function Deposite() {
   } else {
     errorMessage.textContent = "";
   }
+  ////история операций///
+  const progressCards = document.querySelector(".block-4__cards");
+  let progressCard = document.createElement("div");
+  let progressText = document.createElement("p");
+  progressCards.append(progressCard);
+  progressCard.classList.add(
+    "col-3",
+    "m-1",
+    "block-4__progress-card",
+    "rounded-2"
+  );
+  progressCard.append(progressText);
+  progressText.innerHTML = `+${Input}₽`;
+
+  ////прогресс бар////
+  const progressBar = document.querySelector("#block-4__progressbar");
+  let amount = 50000;
+  value = Math.round((Input * 100) / amount);
+  progressBar.setAttribute("value", value);
+  console.log(value);
 }
+
 document.querySelector(".block-4__goal-button").onclick = Deposite;
-
-///////прогресс бар///////
-// const progressBar = document.getElementsByClassName("block-4__progress-bar-active");
-
-// function updateProgressBar(value) {
-//   progressBar.style.width = value + "%";
-// }
-
-// updateProgressBar(25);
